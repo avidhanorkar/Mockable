a75c0824689b6b584c55ebd7cbe33fd4befea9e4
Avishkar Dhanorkar <avidhanorkar23@gmail.com>
2025-11-17 01:40:23 +0530

    Navbar Created

---
+++ frontend/src/Components/Navbar.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='bg-black h-20 w-screen flex flex-row justify-between items-center px-20'>
+            <h1 className='text-white text-3xl font-bold'>Mockable</h1>
+
+            <div className='text-gray-200 flex flex-row gap-5'>
+                <div className="group relative cursor-pointer w-fit">
+                    <p className="text-white/80 group-hover:text-white transition">About</p>
+                    <span className="absolute left-0 -bottom-1 h-[2px] w-0 bg-white transition-all duration-300 group-hover:w-full"></span>
+                </div>
+                <div className="group relative cursor-pointer w-fit">
+                    <p className="text-white/80 group-hover:text-white transition">Pricing</p>
+                    <span className="absolute left-0 -bottom-1 h-[2px] w-0 bg-white transition-all duration-300 group-hover:w-full"></span>
+                </div>
+                <div className="group relative cursor-pointer w-fit">
+                    <p className="text-white/80 group-hover:text-white transition">Contact</p>
+                    <span className="absolute left-0 -bottom-1 h-[2px] w-0 bg-white transition-all duration-300 group-hover:w-full"></span>
+                </div>
+                
+            </div>
+            <div className='flex flex-row gap-2'>
+                <button className='bg-white text-black px-4 py-2 rounded-md font-semibold hover:bg-gray-200'>Sign In</button>
+                <button className='bg-white text-black px-4 py-2 rounded-md font-semibold hover:bg-gray-200'>Sign In</button>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
