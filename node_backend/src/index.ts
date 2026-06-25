bb37fa2c361eff28f5ca552d07df9d33b07f8779
Avishkar Dhanorkar <avidhanorkar23@gmail.com>
2026-06-25 17:32:09 +0530

    feat: initialize Node.js backend server with Express, MongoDB configuration, and Docker support

--- node_backend/src/index.ts
+++ node_backend/src/index.ts
@@ -18,7 +18,7 @@ app.use(cookieParser()); // cookie access
 // Backend PORT 3000
 // 
 const corsOption = {
-  origin: "http://localhost:5173",
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true
 }
 app.use(cors(corsOption));
